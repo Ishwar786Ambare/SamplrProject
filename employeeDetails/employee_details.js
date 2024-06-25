29ad842bd85c45954259778d4633996f73ea109b
Ishwar Ambare <ishwarambare@gmail.com>
2024-06-24 23:15:41 -0400

    message

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -33,6 +33,7 @@ function findEmployeeById(employeeId) {
     }
 }
 
-// function displayEmployees() {
-//     employees.
-// }
+function displayEmployees() {
+    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
