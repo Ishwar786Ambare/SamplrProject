570aaa1172c3e576efc114d7ba8102bf2268f4ea
Ishwar Ambare <ishwarambare@gmail..com>
2024-07-01 17:10:36 +0530

    searchCondition added

--- heath_census/healthAnalysis/health_analysis.js
+++ heath_census/healthAnalysis/health_analysis.js
@@ -116,3 +116,40 @@ function searchCondition() {
 
 btnSearch.addEventListener('click', searchCondition);
 
+
+function searchCondition() {
+    const input = document.getElementById("conditionInput").value.toLowerCase();
+    const resultDiv = document.getElementById("result");
+
+    resultDiv.innerHTML = '';
+    let URL = 'http://127.0.0.1:9999/health_analysis.json'
+    fetch(URL)
+        .then(responce => console.log(responce.json()))
+        .then(data => {
+            const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+            console.log( condition);
+            if (condition) {
+                const symptoms = condition.symptoms.join();
+                const prevention = condition.prevention.join();
+                const treatment = condition.treatment
+
+                resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+                resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+                resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+                resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+
+            } else {
+
+                resultDiv.innerHTML = 'Condition not found.';
+            }
+
+        })
+        .catch((error) => {
+            console.log('Error: ' + error);
+            resultDiv.innerHTML = `An error occurred while fetching data.${error.message}`;
+        })
+}
+
+btnSearch.addEventListener('click', searchCondition);
