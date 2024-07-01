c737a00b64561e88855299a7634d9cdd960cff4e
Ishwar Ambare <ishwarambare@gmail..com>
2024-07-01 14:10:09 +0530

    code formated

--- test.js
+++ test.js
@@ -1,14 +1,47 @@
-// Import Axios
+class Bank {
+    print() {
+        console.log("Welcome to Banking system");
+    }
+}
 
-const axios = require('axios');
+class SBI extends Bank {
 
-// Make a GET request
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    // Handle successful response
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    // Handle error
-    console.error('Error:', error);
-  });
+    constructor(bid, name) {
+        super();
+        this.bid = bid; 
+        this.name = name;
+        console.log("Inside Child constructor");
+}
+}
+
+
+let sbi = new SBI(101, "SBI Noida")
+
+sbi.print();
+
+
+// const conditionsCount = {
+//   Diabetes: 0,
+//   Thyroid: 0,
+//   "High Blood Pressure": 0,
+// }
+
+// const genderConditionsCount = {
+//   Male:{
+//       Diabetes: 0,
+//       Thyroid: 0,
+//       "High Blood Pressure": 0,
+//   },
+//   Female:{
+//       Diabetes: 0,
+//       Thyroid: 0,
+//       "High Blood Pressure": 0,
+//   }
+// }
+p1 = new Promise((resolve, reject) => setTimeout(() => resolve(1), 3000))
+p2 = new Promise((resolve, reject) => setTimeout(() => resolve(2), 2000))
+p3 = new Promise((resolve, reject) => setTimeout(() => resolve(3), 1000) )
+
+Promise.all([p1,p2,p3]).then(values => { console.log(values); }) 
+.catch(error => {console.error(error. message) }); 
+
